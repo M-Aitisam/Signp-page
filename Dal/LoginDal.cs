3d9c5520d53bc279f817cb67254675338bc26909
M-Aitisam <aitisamahmed24@gmail.com>
2024-08-05 13:35:54 +0500

    Add the Content  where user can successfully SignUp  Login  and then  go to the index Page  Successfully and then  the data   successfully show in the database

--- Dal/LoginDal.cs
+++ Dal/LoginDal.cs
@@ -1,42 +1,71 @@
-﻿    using Model;
-    using System.Data.SqlClient;
+﻿using Model;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
 
-    namespace Dal
+namespace Dal
+{
+    public class LoginDal
     {
-        public class LoginDal
+        public void RegisterUser(LoginModel user)
         {
-            public void RegisterUser(LoginModel user)
+            using (SqlConnection conn = new SqlConnection(DBHelper.GetConnection()))
             {
-                using (SqlConnection conn = new SqlConnection(DBHelper.GetConnection()))
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO Users (Username, Password) VALUES (@Username, @Password)", conn))
                 {
-                    conn.Open();
-                    SqlCommand cmd = new SqlCommand("INSERT INTO Users (Username, Password) VALUES (@Username, @Password)", conn);
                     cmd.Parameters.AddWithValue("@Username", user.Username);
                     cmd.Parameters.AddWithValue("@Password", user.Password);
                     cmd.ExecuteNonQuery();
                 }
             }
+        }
 
-            public LoginModel? LoginUser(string username, string password)
+        public LoginModel? LoginUser(string username, string password)
+        {
+            using (SqlConnection conn = new SqlConnection(DBHelper.GetConnection()))
             {
-                using (SqlConnection conn = new SqlConnection(DBHelper.GetConnection()))
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM Users WHERE Username = @Username AND Password = @Password", conn))
                 {
-                    conn.Open();
-                    SqlCommand cmd = new SqlCommand("SELECT * FROM Users WHERE Username = @Username AND Password = @Password", conn);
                     cmd.Parameters.AddWithValue("@Username", username);
                     cmd.Parameters.AddWithValue("@Password", password);
-                    SqlDataReader reader = cmd.ExecuteReader();
-                    if (reader.Read())
+                    using (SqlDataReader reader = cmd.ExecuteReader())
                     {
-                        return new LoginModel
+                        if (reader.Read())
                         {
-                            Id = (int)reader["Id"],
-                            Username = (string)reader["Username"],
-                            Password = (string)reader["Password"]
-                        };
+                            return new LoginModel
+                            {
+                                Id = (int)reader["Id"],
+                                Username = (string)reader["Username"],
+                                Password = (string)reader["Password"]
+                            };
+                        }
+                        return null;
                     }
-                    return null;
                 }
             }
         }
+
+        public async Task<bool> LogoutUser(string username)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(DBHelper.GetConnection()))
+                {
+                    await conn.OpenAsync();
+                    using (SqlCommand cmd = new SqlCommand("DELETE FROM Users WHERE Username = @Username", conn))
+                    {
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        int rowsAffected = await cmd.ExecuteNonQueryAsync();
+                        return rowsAffected > 0;
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                // Log exception
+                return false;
+            }
+        }
     }
+}
