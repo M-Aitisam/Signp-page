752fcc70c56ff8e3b5448d496e05aa71de5f754d
M-Aitisam <160387918+M-Aitisam@users.noreply.github.com>
2024-08-05 13:43:20 +0500

    Add files via upload
     Here is the data base sql file

---
+++ SignUp.sql
@@ -0,0 +1,14 @@
+create database signup
+
+use [signup]
+
+CREATE TABLE Users
+(
+    Id INT PRIMARY KEY IDENTITY,
+    Username NVARCHAR(50) NOT NULL,
+    Password NVARCHAR(50) NOT NULL
+);
+
+select* from Users
+
+
